1d69a680947bd2bcbfe9a31e1e378cf69d4a62e7
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-26 14:52:04 +0100

    Update RevisitingSelect-2.sql

--- HackerRank/RevisitingSelect-2.sql
+++ HackerRank/RevisitingSelect-2.sql
@@ -1,10 +1,17 @@
 
 -- Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
 -- The CountryCode for America is USA.
+-- The CITY table is described as follows:
 
-The CITY table is described as follows:
+| Field       | Type            |
+|-------------|-----------------|
+| ID          | NUMBER          |
+| NAME        | VARCHAR(17)     |
+| COUNTRYCODE | VARCHAR2(3)     |
+| DISTRICT    | VARCHAR2(20)    |
+| POPULATION  | NUMBER          |
 
-
+  
 SELECT
 NAME
 FROM CITY
