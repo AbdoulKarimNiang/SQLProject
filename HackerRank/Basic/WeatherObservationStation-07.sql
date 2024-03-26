f2f19dc787f0b25255ae1b50edb75726e12cd4ce
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-26 17:08:10 +0100

    Create WeatherObservationStation-07.sql

---
+++ HackerRank/Basic/WeatherObservationStation-07.sql
@@ -0,0 +1,17 @@
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) 
+-- from STATION. Your result cannot contain duplicates.
+
+--Input Format
+
+| Field    | Type         |
+|----------|--------------|
+| ID       | NUMBER       |
+| CITY     | VARCHAR(21)  |
+| STATE    | VARCHAR(2)   |
+| LAT_N    | NUMBER       |
+| LONG_W   | NUMBER       |
+
+
+-- The STATION table is described as follows:
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
