642338a7396308682ecbe02844df874d85dca155
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-26 14:54:29 +0100

    Create SelectAll.sql

---
+++ HackerRank/Basic/SelectAll.sql
@@ -0,0 +1,13 @@
+-- Query all columns (attributes) for every row in the CITY table.
+
+| Field       | Type            |
+|-------------|-----------------|
+| ID          | NUMBER          |
+| NAME        | VARCHAR(17)     |
+| COUNTRYCODE | VARCHAR2(3)     |
+| DISTRICT    | VARCHAR2(20)    |
+| POPULATION  | NUMBER          |
+
+
+SELECT *
+FROM CITY
