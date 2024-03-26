e4765380705d61a0ec1045a6cf13116510c50a49
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-26 16:09:30 +0100

    Create WeatherObservationStation1.sql

---
+++ HackerRank/Basic/WeatherObservationStation1.sql
@@ -0,0 +1,16 @@
+-- Query a list of CITY and STATE from the STATION table.
+-- The STATION table is described as follows:
+
+| Field    | Type         |
+|----------|--------------|
+| ID       | NUMBER       |
+| CITY     | VARCHAR(21)  |
+| STATE    | VARCHAR(2)   |
+| LAT_N    | NUMBER       |
+| LONG_W   | NUMBER       |
+
+
+SELECT
+CITY,
+STATE
+FROM STATION;
