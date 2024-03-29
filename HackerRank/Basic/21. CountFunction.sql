31eca454729c85a6ce58626a358023a9743fb5e6
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-29 19:57:44 +0100

    Create 21. CountFunction.sql

---
+++ HackerRank/Basic/21. CountFunction.sql
@@ -0,0 +1,11 @@
+-- Query a count of the number of cities in CITY having a Population larger than 100.000.
+
+-- Input Format
+
+-- The CITY table is described as follows:
+
+SELECT
+COUNT(NAME)
+FROM CITY
+WHERE POPULATION >100000;
+  
