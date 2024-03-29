82989b1245252971d39cd74be1aecdc759b335bc
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-29 19:50:00 +0100

    Update 19.EmployeeSalaries.sql

--- HackerRank/Basic/19.EmployeeSalaries.sql
+++ HackerRank/Basic/19.EmployeeSalaries.sql
@@ -1,13 +1,16 @@
 -- Write a query that prints a list of employee names (i.e.: the name attribute) 
--- for employees in Employee having a salary greater than  per month 
--- who have been employees for less than  months. Sort your result by ascending employee_id.
+-- for employees in Employee having a salary greater than  2.000 per month 
+-- who have been employees for less than 10 months. 
+-- Sort your result by ascending employee_id.
+
 
 -- Input Format
-
--- The Employee table containing employee data for a company is described as follows:
-
-
-
+| Field       | Type          |
+|-------------|---------------|
+| employee_id | INTEGER       |
+| name        | STRING        |
+| months      | INTEGER       |
+| salary      | INTEGER       |
 
 SELECT name
 FROM employee
