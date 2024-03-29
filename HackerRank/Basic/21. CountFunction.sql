f8c8d69c906c873d065bf8ab76a6d3969051f976
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-29 19:58:24 +0100

    Update 21. CountFunction.sql

--- HackerRank/Basic/21. CountFunction.sql
+++ HackerRank/Basic/21. CountFunction.sql
@@ -1,6 +1,13 @@
 -- Query a count of the number of cities in CITY having a Population larger than 100.000.
 
 -- Input Format
+| Field       | Type            |
+|-------------|-----------------|
+| ID          | NUMBER          |
+| NAME        | VARCHAR(17)     |
+| COUNTRYCODE | VARCHAR2(3)     |
+| DISTRICT    | VARCHAR2(20)    |
+| POPULATION  | NUMBER          |
 
 -- The CITY table is described as follows:
 
