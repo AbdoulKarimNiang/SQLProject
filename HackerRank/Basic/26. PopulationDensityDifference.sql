f90eb59bb3846212f0582b38838e68bbce69cf8f
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-31 18:01:32 +0200

    Create 26. PopulationDensityDifference.sql

---
+++ HackerRank/Basic/26. PopulationDensityDifference.sql
@@ -0,0 +1,16 @@
+-- Query the difference between the maximum and minimum populations in CITY.
+
+-- Input Format
+
+-- The CITY table is described as follows:
+
+| Field       | Type            |
+|-------------|-----------------|
+| ID          | NUMBER          |
+| NAME        | VARCHAR(17)     |
+| COUNTRYCODE | VARCHAR2(3)     |
+| DISTRICT    | VARCHAR2(20)    |
+| POPULATION  | NUMBER          |
+
+SELECT MAX(Population) - MIN(population)
+FROM city;
