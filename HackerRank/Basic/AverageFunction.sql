bc4f1ebc630c61caa2ac57956dcbfdaffeeeb4d8
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-31 17:54:46 +0200

    Create AverageFunction.sql

---
+++ HackerRank/Basic/AverageFunction.sql
@@ -0,0 +1,12 @@
+-- Query the average population of all cities in CITY where District is California.
+
+-- Input Format
+
+-- The CITY table is described as follows
+
+
+
+
+SELECT AVG(Population)
+FROM city
+WHERE district = 'California';
