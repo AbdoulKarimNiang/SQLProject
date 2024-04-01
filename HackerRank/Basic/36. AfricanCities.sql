3e2c0de984beba93f64b9b122a5e73a4af8b60a2
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 10:46:28 +0200

    Update 36. AfricanCities.sql

--- HackerRank/Basic/36. AfricanCities.sql
+++ HackerRank/Basic/36. AfricanCities.sql
@@ -1,13 +1,40 @@
 -- Given the CITY and COUNTRY tables, 
 -- query the names of all cities where the CONTINENT is 'Africa'.
 
--- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+-- Note: CITY.CountryCode and COUNTRY. Code are matching key columns.
 
 -- Input Format
 
 -- The CITY and COUNTRY tables are described as follows:
 
+| Field           | Type            |
+|---------        |---------------- |
+| ID              | NUMBER          |
+| NAME            | VARCHAR2(17)    |
+| CONTRYCODE      | VARCHAR2(3)     |
+| DISTRICT        | VARCHAR2(20)     |
+| POPULATION      | NUMBER          |
 
+
+-- COUNTRY
+  
+| Field           | Type            |
+|---------        |---------------- |
+| CODE            | VARCHAR2(3)     |
+| NAME            | VARCHAR2(44)    |
+| CONTINENTE      | VARCHAR2(13)    |
+| REGION          | VARCHAR2(5)     |
+| SURFACEAREA     | NUMBER          |
+| INDEPYEAR       | VARCHAR2(4)     |
+| POPULATION      | NUMBER          |
+| LIFEEXPENTANCY  | VARCHAR2(4)     |
+| GNP             | NUMBER          |
+| GNPOLD          | VARCHAR2(9)     |
+| LOCALNAME       | VARCHAR2(44)    |
+| GOVERNMENTFORM  | VARCHAR2(44)    |
+| HEADOFSTATE     | VARCHAR2(32)    |
+| CAPITAL         | VARCHAR2(4)     |
+| CODE2           | VARCHAR2(2)    
 
 
 
