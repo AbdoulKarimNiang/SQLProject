78ae7c2847518be2f0b526828453f1e6f5272591
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 07:51:34 +0200

    Create 32. WeatherObservationStation-15.sql

---
+++ HackerRank/Basic/32. WeatherObservationStation-15.sql
@@ -0,0 +1,23 @@
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 38.7880.
+-- Round your answer to  decimal places.
+
+-- Input Format
+
+-- The STATION table is described as follows:
+
+  
+| Field   | Type        |
+|---------|-------------|
+| ID      | NUMBER      |
+| CITY    | VARCHAR(21) |
+| STATE   | VARCHAR(2)  |
+| LAT_N   | NUMBER      |
+| LONG_W  | NUMBER      |
+
+
+SELECT 
+ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N < 137.2345 
+ORDER BY LAT_N DESC 
+LIMIT 1;
