cf4ed63dd39407c2cbf1b8d74ee76089ee34093f
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 07:42:16 +0200

    Create 29. WeartherObservationStation-2.sql

---
+++ HackerRank/Basic/29. WeartherObservationStation-2.sql
@@ -0,0 +1,17 @@
+-- Query the following two values from the STATION table:
+
+-- The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+-- The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+-- Input Format
+
+-- The STATION table is described as follows:
+
+
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+-- Your results must be in the form:
+-- lat lon
+
+
+-- where lat is the sum of all values in LAT_N and lon is the sum of all values in LONG_W.
+-- Both results must be rounded to a scale of 2 decimal places.
