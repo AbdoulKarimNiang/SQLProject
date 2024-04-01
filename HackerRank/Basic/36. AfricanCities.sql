94573bd836352859dfbe295c56b51d9bcea3b85c
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 10:46:07 +0200

    Create 36. AfricanCities.sql

---
+++ HackerRank/Basic/36. AfricanCities.sql
@@ -0,0 +1,17 @@
+-- Given the CITY and COUNTRY tables, 
+-- query the names of all cities where the CONTINENT is 'Africa'.
+
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+-- Input Format
+
+-- The CITY and COUNTRY tables are described as follows:
+
+
+
+
+
+
+SELECT City.Name 
+FROM City, Country 
+WHERE City.CountryCode = Country.Code AND Continent = 'Africa' ;
