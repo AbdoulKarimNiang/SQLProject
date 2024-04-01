bea95bc59b072acfe8f5947f579280eae322b505
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 07:56:43 +0200

    Update 32. WeatherObservationStation-15.sql

--- HackerRank/Basic/32. WeatherObservationStation-15.sql
+++ HackerRank/Basic/32. WeatherObservationStation-15.sql
@@ -1,5 +1,5 @@
 -- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345.
--- Round your answer to 2 decimal places.4
+-- Round your answer to 4 decimal places.
 -- Input Format
 
 -- The STATION table is described as follows:
