a3d4d07da8685e2950033e4bb17d48534b37d40b
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 07:43:15 +0200

    Update 29. WeartherObservationStation-2.sql

--- HackerRank/Basic/29. WeartherObservationStation-2.sql
+++ HackerRank/Basic/29. WeartherObservationStation-2.sql
@@ -2,9 +2,19 @@
 
 -- The sum of all values in LAT_N rounded to a scale of 2 decimal places.
 -- The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+
+
 -- Input Format
 
 -- The STATION table is described as follows:
+
+| Field   | Type        |
+|---------|-------------|
+| ID      | NUMBER      |
+| CITY    | VARCHAR(21) |
+| STATE   | VARCHAR(2)  |
+| LAT_N   | NUMBER      |
+| LONG_W  | NUMBER      |
 
 
 
@@ -15,3 +25,9 @@
 
 -- where lat is the sum of all values in LAT_N and lon is the sum of all values in LONG_W.
 -- Both results must be rounded to a scale of 2 decimal places.
+
+SELECT 
+ROUND(SUM(LAT_N),2),
+ROUND(SUM(LONG_W),2)
+FROM
+station;
