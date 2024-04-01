fb9def92ce258d1209204784efdc1e5d5e4ba455
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-04-01 07:54:34 +0200

    Create 33. WeatherObservationStation-16.sql

---
+++ HackerRank/Basic/33. WeatherObservationStation-16.sql
@@ -0,0 +1,22 @@
+-- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7880.
+-- Round your answer to 2 decimal places.
+
+-- Input Format
+
+-- The STATION table is described as follows:
+
+ 
+| Field   | Type        |
+|---------|-------------|
+| ID      | NUMBER      |
+| CITY    | VARCHAR(21) |
+| STATE   | VARCHAR(2)  |
+| LAT_N   | NUMBER      |
+| LONG_W  | NUMBER      |
+
+SELECT
+ROUND(LAT_N,4)
+FROM STATION
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N ASC
+LIMIT 1;
